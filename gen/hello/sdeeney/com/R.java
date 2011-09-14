15c5ebda8cf0dfd33bbfa92bdcbb069a618de22c
Steven Deeney <vtboarder89@gmail.com>
2011-09-13 23:51:40 -0400

    created hello world and added a button and edittext to change the displayed text. also added Toasts to say when the different events of an activity occur and when the text is changed

--- gen/hello/sdeeney/com/R.java
+++ gen/hello/sdeeney/com/R.java
@@ -14,8 +14,9 @@ public final class R {
         public static final int icon=0x7f020000;
     }
     public static final class id {
-        public static final int button1=0x7f050001;
-        public static final int editText1=0x7f050000;
+        public static final int button=0x7f050001;
+        public static final int editText=0x7f050000;
+        public static final int tv=0x7f050002;
     }
     public static final class layout {
         public static final int main=0x7f030000;
--- src/hello/sdeeney/com/HelloAndroidActivity.java
+++ src/hello/sdeeney/com/HelloAndroidActivity.java
@@ -2,12 +2,100 @@ package hello.sdeeney.com;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.View.OnKeyListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class HelloAndroidActivity extends Activity {
+	public CharSequence text;
+	private TextView tv;
+	
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        Toast.makeText(this, "onCreate", Toast.LENGTH_SHORT).show();
+        Button button = (Button) findViewById(R.id.button);
+        tv = (TextView) findViewById(R.id.tv);
+        final EditText textBox = (EditText) findViewById(R.id.editText);
+        
+        textBox.setOnKeyListener(new OnKeyListener(){
+        	
+        	public boolean onKey(View v, int keyCode, KeyEvent event)
+        	{
+        		if(event.getAction()==KeyEvent.ACTION_DOWN && keyCode==KeyEvent.KEYCODE_ENTER)
+        		{
+        			text = textBox.getText();
+        			ChangeText();
+        			return true;
+        		}
+        		return false;
+        	}
+        });
+        button.setOnClickListener(new OnClickListener(){
+        	public void onClick(View view)
+        	{
+        		text = textBox.getText();
+    			ChangeText();
+        	}
+        });
     }
+    @Override
+    public void onStart()
+    {
+    	super.onStart();
+    	Toast.makeText(this, "onStart", Toast.LENGTH_SHORT).show();
+    }
+    @Override
+    public void onResume()
+    {
+    	super.onResume();
+    	Toast.makeText(this, "onResume", Toast.LENGTH_SHORT).show();
+    }
+    @Override
+    public void onRestart()
+    {
+    	super.onRestart();
+    	Toast.makeText(this, "onRestart", Toast.LENGTH_SHORT).show();
+    }
+    @Override
+    public void onPause()
+    {
+    	Toast.makeText(this, "onPause", Toast.LENGTH_SHORT).show();
+    	super.onPause();
+    }
+    @Override
+    public void onStop()
+    {
+    	Toast.makeText(this, "onStop", Toast.LENGTH_SHORT).show();
+    	super.onStop();
+    }
+    @Override
+    public void onDestroy()
+    {
+    	Toast.makeText(this, "onDestroy", Toast.LENGTH_SHORT).show();
+    	super.onDestroy();
+    }
+    
+    private void ChangeText()
+    {
+    	tv.setText(text);
+    	Toast.makeText(this, "Changed Text : " + text, Toast.LENGTH_SHORT).show();
+    }
+    private void SaveState()
+    {
+    
+    }
+    private void RestoreState()
+    {
+    	
+    }
+    
+    
 }
